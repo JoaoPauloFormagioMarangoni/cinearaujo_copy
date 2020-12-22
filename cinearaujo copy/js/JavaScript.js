66ab5a163b0d34c4d2bdec85d0a3277c87ea0838
JoaoPauloFormagioMarangoni <joao_paulinh@hotmail.com>
2020-12-22 18:20:04 -0300

    Commit
    Cópia do site cine araujo

---
+++ cinearaujo copy/js/JavaScript.js
@@ -0,0 +1,27 @@
+function menu(num) {
+    if(num == 1) {
+        document.getElementById("primenu").src = "IMG/calendario2.png";
+    }
+    if(num == 2) {
+        document.getElementById("primenu").src = "IMG/calendario.png";
+    }
+    if(num == 3) {
+        document.getElementById("segmenu").src = "IMG/padeiro2.png";
+    }
+    if(num == 4) {
+        document.getElementById("segmenu").src = "IMG/padeiro.png";
+    }
+    if(num == 5) {
+        document.getElementById("termenu").src = "IMG/sorrir2.png";
+    }
+    if(num == 6) {
+        document.getElementById("termenu").src = "IMG/sorrir.png";
+    }
+    if(num == 7) {
+        document.getElementById("quamenu").src = "IMG/contatos2.png";
+    }
+    if(num == 8) {
+        document.getElementById("quamenu").src = "IMG/contatos.png";
+    }
+    
+}
